55408850aa134c1cb02f31901870a1b219ffd9b7
luna <luna@wvvw.me>
2024-01-26 14:40:50 +1030

    feat: more backgrounds

--- src/app/components/screenshot-tool.tsx
+++ src/app/components/screenshot-tool.tsx
@@ -365,15 +365,158 @@ export const ScreenshotTool = () => {
   useEffect(() => {
     // Preload the images
     images.current = [
-      '/backgrounds/imlunahey_the_number_of_times_a_tweet_has_been_shared_over_the__0b00e323-4137-44b0-914c-c3a89e8b3b0f.webp',
-      '/backgrounds/imlunahey_the_number_of_times_a_tweet_has_been_shared_over_the__50e358ee-16df-4474-999d-aedb4118bd91.webp',
-      '/backgrounds/imlunahey_the_number_of_times_a_tweet_has_been_shared_over_the__b53afe4d-76ec-499a-be92-a58f65bbf7bd.webp',
-      '/backgrounds/imlunahey_the_number_of_times_a_tweet_has_been_shared_over_the__e9c05a6a-bcc1-4e7d-9908-90fe3ab11967.webp',
-    ].map((src) => {
-      const image = new Image();
-      image.src = src;
-      return image;
-    });
+      'imlunahey_3d_wallpapers_of_water_splashes_in_the_ocean_in_the_b327bffc-94c7-4314-a669-c86bfab3a17a_0.png',
+      'imlunahey_3d_wallpapers_of_water_splashes_in_the_ocean_in_the_b327bffc-94c7-4314-a669-c86bfab3a17a_1.png',
+      'imlunahey_3d_wallpapers_of_water_splashes_in_the_ocean_in_the_b327bffc-94c7-4314-a669-c86bfab3a17a_2.png',
+      'imlunahey_3d_wallpapers_of_water_splashes_in_the_ocean_in_the_b327bffc-94c7-4314-a669-c86bfab3a17a_3.png',
+      'imlunahey_a_beautiful_image_of_ocean_colors_in_a_watery_view__ef7faa5a-66af-4389-8741-083b07da80ef_0.png',
+      'imlunahey_a_beautiful_image_of_ocean_colors_in_a_watery_view__ef7faa5a-66af-4389-8741-083b07da80ef_1.png',
+      'imlunahey_a_beautiful_image_of_ocean_colors_in_a_watery_view__ef7faa5a-66af-4389-8741-083b07da80ef_2.png',
+      'imlunahey_a_beautiful_image_of_ocean_colors_in_a_watery_view__ef7faa5a-66af-4389-8741-083b07da80ef_3.png',
+      'imlunahey_a_splash_of_blue_orange_and_green_over_water_waves__de296173-e858-4410-a040-ff679eff9b17_0.png',
+      'imlunahey_a_splash_of_blue_orange_and_green_over_water_waves__de296173-e858-4410-a040-ff679eff9b17_1.png',
+      'imlunahey_a_splash_of_blue_orange_and_green_over_water_waves__de296173-e858-4410-a040-ff679eff9b17_2.png',
+      'imlunahey_a_splash_of_blue_orange_and_green_over_water_waves__de296173-e858-4410-a040-ff679eff9b17_3.png',
+      'imlunahey_abstract_sea_in_blue_and_orange_with_waves_in_the_s_70d32e86-fd8a-4abd-b518-0c2eaf7345a6_0.png',
+      'imlunahey_abstract_sea_in_blue_and_orange_with_waves_in_the_s_70d32e86-fd8a-4abd-b518-0c2eaf7345a6_1.png',
+      'imlunahey_abstract_sea_in_blue_and_orange_with_waves_in_the_s_70d32e86-fd8a-4abd-b518-0c2eaf7345a6_2.png',
+      'imlunahey_abstract_sea_in_blue_and_orange_with_waves_in_the_s_70d32e86-fd8a-4abd-b518-0c2eaf7345a6_3.png',
+      'imlunahey_an_abstract_image_of_sea_waves_and_splashes_in_the__6b58323b-7223-4fcd-a0c2-a83442459492_0.png',
+      'imlunahey_an_abstract_image_of_sea_waves_and_splashes_in_the__6b58323b-7223-4fcd-a0c2-a83442459492_1.png',
+      'imlunahey_an_abstract_image_of_sea_waves_and_splashes_in_the__6b58323b-7223-4fcd-a0c2-a83442459492_2.png',
+      'imlunahey_an_abstract_image_of_sea_waves_and_splashes_in_the__6b58323b-7223-4fcd-a0c2-a83442459492_3.png',
+      'imlunahey_an_image_of_beautiful_water_wave_in_the_style_of_da_7efdf1ee-8058-4114-9c40-d3f922becb56_0.png',
+      'imlunahey_an_image_of_beautiful_water_wave_in_the_style_of_da_7efdf1ee-8058-4114-9c40-d3f922becb56_1.png',
+      'imlunahey_an_image_of_beautiful_water_wave_in_the_style_of_da_7efdf1ee-8058-4114-9c40-d3f922becb56_2.png',
+      'imlunahey_an_image_of_beautiful_water_wave_in_the_style_of_da_7efdf1ee-8058-4114-9c40-d3f922becb56_3.png',
+      'imlunahey_cloud_pattern_wallpaper_2_top_25_android_wallpapers_69a02d2a-9405-4f6a-8a23-ca20f5be8aa6_0.png',
+      'imlunahey_cloud_pattern_wallpaper_2_top_25_android_wallpapers_69a02d2a-9405-4f6a-8a23-ca20f5be8aa6_1.png',
+      'imlunahey_cloud_pattern_wallpaper_2_top_25_android_wallpapers_69a02d2a-9405-4f6a-8a23-ca20f5be8aa6_2.png',
+      'imlunahey_cloud_pattern_wallpaper_2_top_25_android_wallpapers_69a02d2a-9405-4f6a-8a23-ca20f5be8aa6_3.png',
+      'imlunahey_cool_images_of_waves_in_the_style_of_colorful_turbu_34f5ad58-7ace-49c5-a007-2c7f554cfde4_0.png',
+      'imlunahey_cool_images_of_waves_in_the_style_of_colorful_turbu_34f5ad58-7ace-49c5-a007-2c7f554cfde4_1.png',
+      'imlunahey_cool_images_of_waves_in_the_style_of_colorful_turbu_34f5ad58-7ace-49c5-a007-2c7f554cfde4_2.png',
+      'imlunahey_cool_images_of_waves_in_the_style_of_colorful_turbu_34f5ad58-7ace-49c5-a007-2c7f554cfde4_3.png',
+      'imlunahey_splashing_ocean_waves_in_the_air_desktop_wallpaper__d88c4170-bbf2-4a08-99e1-e40de50e1521_0.png',
+      'imlunahey_splashing_ocean_waves_in_the_air_desktop_wallpaper__d88c4170-bbf2-4a08-99e1-e40de50e1521_1.png',
+      'imlunahey_splashing_ocean_waves_in_the_air_desktop_wallpaper__d88c4170-bbf2-4a08-99e1-e40de50e1521_2.png',
+      'imlunahey_splashing_ocean_waves_in_the_air_desktop_wallpaper__d88c4170-bbf2-4a08-99e1-e40de50e1521_3.png',
+      'imlunahey_the_abstract_image_of_pink_and_blue_colors_in_the_s_aa778ece-ee52-4e4f-9d72-9f526a3e3f6b_0.png',
+      'imlunahey_the_abstract_image_of_pink_and_blue_colors_in_the_s_aa778ece-ee52-4e4f-9d72-9f526a3e3f6b_1.png',
+      'imlunahey_the_abstract_image_of_pink_and_blue_colors_in_the_s_aa778ece-ee52-4e4f-9d72-9f526a3e3f6b_2.png',
+      'imlunahey_the_abstract_image_of_pink_and_blue_colors_in_the_s_aa778ece-ee52-4e4f-9d72-9f526a3e3f6b_3.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_04a6fa6c-ba9e-427c-aef0-0c83a0110d2d_0.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_04a6fa6c-ba9e-427c-aef0-0c83a0110d2d_1.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_04a6fa6c-ba9e-427c-aef0-0c83a0110d2d_2.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_04a6fa6c-ba9e-427c-aef0-0c83a0110d2d_3.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_0527e128-3775-430f-ad21-ef2e1303192e_0.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_0527e128-3775-430f-ad21-ef2e1303192e_1.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_0527e128-3775-430f-ad21-ef2e1303192e_2.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_0527e128-3775-430f-ad21-ef2e1303192e_3.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_1b273d1d-8dd1-4d8a-89d4-1b2481fed417_0.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_1b273d1d-8dd1-4d8a-89d4-1b2481fed417_1.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_1b273d1d-8dd1-4d8a-89d4-1b2481fed417_2.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_1b273d1d-8dd1-4d8a-89d4-1b2481fed417_3.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_2479cb68-786b-4b45-bd3b-2d61eb736831_0.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_2479cb68-786b-4b45-bd3b-2d61eb736831_1.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_2479cb68-786b-4b45-bd3b-2d61eb736831_2.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_2479cb68-786b-4b45-bd3b-2d61eb736831_3.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_25bf7ecb-3ce7-497f-80f1-5d694a3425b2_0.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_25bf7ecb-3ce7-497f-80f1-5d694a3425b2_1.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_25bf7ecb-3ce7-497f-80f1-5d694a3425b2_2.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_25bf7ecb-3ce7-497f-80f1-5d694a3425b2_3.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_2a03da6e-078e-4b70-a90d-0baf6bbb5830_0.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_2a03da6e-078e-4b70-a90d-0baf6bbb5830_1.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_2a03da6e-078e-4b70-a90d-0baf6bbb5830_2.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_2a03da6e-078e-4b70-a90d-0baf6bbb5830_3.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_2e65175c-892a-4cf5-8c8b-77172aaaf300_0.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_2e65175c-892a-4cf5-8c8b-77172aaaf300_1.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_2e65175c-892a-4cf5-8c8b-77172aaaf300_2.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_2e65175c-892a-4cf5-8c8b-77172aaaf300_3.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_445683ba-6ff5-42b6-8496-895c9b3ed4ee_0.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_445683ba-6ff5-42b6-8496-895c9b3ed4ee_1.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_445683ba-6ff5-42b6-8496-895c9b3ed4ee_2.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_445683ba-6ff5-42b6-8496-895c9b3ed4ee_3.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_46e72589-4a4e-473a-97a7-e3baef0ada27_0.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_46e72589-4a4e-473a-97a7-e3baef0ada27_1.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_46e72589-4a4e-473a-97a7-e3baef0ada27_2.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_46e72589-4a4e-473a-97a7-e3baef0ada27_3.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_6609f307-8a2c-4fd1-bbbf-3769e8be1eca_0.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_6609f307-8a2c-4fd1-bbbf-3769e8be1eca_1.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_6609f307-8a2c-4fd1-bbbf-3769e8be1eca_2.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_6609f307-8a2c-4fd1-bbbf-3769e8be1eca_3.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_683efc08-5e6f-4199-9335-e02ca1acd8ef_0.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_683efc08-5e6f-4199-9335-e02ca1acd8ef_1.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_683efc08-5e6f-4199-9335-e02ca1acd8ef_2.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_683efc08-5e6f-4199-9335-e02ca1acd8ef_3.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_6b0a1673-c0b2-4eb0-98e4-4fbac039724c_0.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_6b0a1673-c0b2-4eb0-98e4-4fbac039724c_1.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_6b0a1673-c0b2-4eb0-98e4-4fbac039724c_2.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_6b0a1673-c0b2-4eb0-98e4-4fbac039724c_3.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_7177b260-4622-4e98-8a92-b635a52962c1_0.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_7177b260-4622-4e98-8a92-b635a52962c1_1.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_7177b260-4622-4e98-8a92-b635a52962c1_2.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_7177b260-4622-4e98-8a92-b635a52962c1_3.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_77326f95-83d2-4dd7-9c5a-0eb82cc87103_0.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_77326f95-83d2-4dd7-9c5a-0eb82cc87103_1.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_77326f95-83d2-4dd7-9c5a-0eb82cc87103_2.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_77326f95-83d2-4dd7-9c5a-0eb82cc87103_3.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_b26a1cc2-234e-4173-8932-f880b916d29e_0.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_b26a1cc2-234e-4173-8932-f880b916d29e_1.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_b26a1cc2-234e-4173-8932-f880b916d29e_2.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_b26a1cc2-234e-4173-8932-f880b916d29e_3.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_d2f6a42b-9fa0-4193-bea5-250cbb736ace_0.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_d2f6a42b-9fa0-4193-bea5-250cbb736ace_1.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_d2f6a42b-9fa0-4193-bea5-250cbb736ace_2.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_d2f6a42b-9fa0-4193-bea5-250cbb736ace_3.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_d3e12350-7e94-4830-9703-88fb4fb63a96_0.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_d3e12350-7e94-4830-9703-88fb4fb63a96_1.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_d3e12350-7e94-4830-9703-88fb4fb63a96_2.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_d3e12350-7e94-4830-9703-88fb4fb63a96_3.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_db993c2d-5bd2-4e43-bacf-eb71c349b769_0.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_db993c2d-5bd2-4e43-bacf-eb71c349b769_1.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_db993c2d-5bd2-4e43-bacf-eb71c349b769_2.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_db993c2d-5bd2-4e43-bacf-eb71c349b769_3.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_ed98f4ad-6222-4439-8407-5c93d6ed538a_0.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_ed98f4ad-6222-4439-8407-5c93d6ed538a_1.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_ed98f4ad-6222-4439-8407-5c93d6ed538a_2.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_ed98f4ad-6222-4439-8407-5c93d6ed538a_3.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_f5e93809-6a21-4094-bf47-34eec6d1cef2_0.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_f5e93809-6a21-4094-bf47-34eec6d1cef2_1.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_f5e93809-6a21-4094-bf47-34eec6d1cef2_2.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_th_f5e93809-6a21-4094-bf47-34eec6d1cef2_3.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_the__0b00e323-4137-44b0-914c-c3a89e8b3b0f.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_the__0b00e323-4137-44b0-914c-c3a89e8b3b0f.webp',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_the__50e358ee-16df-4474-999d-aedb4118bd91.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_the__50e358ee-16df-4474-999d-aedb4118bd91.webp',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_the__b53afe4d-76ec-499a-be92-a58f65bbf7bd.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_the__b53afe4d-76ec-499a-be92-a58f65bbf7bd.webp',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_the__e9c05a6a-bcc1-4e7d-9908-90fe3ab11967.png',
+      'imlunahey_the_number_of_times_a_tweet_has_been_shared_over_the__e9c05a6a-bcc1-4e7d-9908-90fe3ab11967.webp',
+      'imlunahey_the_water_is_showing_some_pink_tints_in_the_style_o_0da1c8e2-a91f-4d85-8929-8635322dbef2_0.png',
+      'imlunahey_the_water_is_showing_some_pink_tints_in_the_style_o_0da1c8e2-a91f-4d85-8929-8635322dbef2_1.png',
+      'imlunahey_the_water_is_showing_some_pink_tints_in_the_style_o_0da1c8e2-a91f-4d85-8929-8635322dbef2_2.png',
+      'imlunahey_the_water_is_showing_some_pink_tints_in_the_style_o_0da1c8e2-a91f-4d85-8929-8635322dbef2_3.png',
+      'imlunahey_the_water_is_showing_some_pink_tints_in_the_style_o_11444df2-6dd4-4fb9-b33a-eda37e15f887_0.png',
+      'imlunahey_the_water_is_showing_some_pink_tints_in_the_style_o_11444df2-6dd4-4fb9-b33a-eda37e15f887_1.png',
+      'imlunahey_the_water_is_showing_some_pink_tints_in_the_style_o_11444df2-6dd4-4fb9-b33a-eda37e15f887_2.png',
+      'imlunahey_the_water_is_showing_some_pink_tints_in_the_style_o_11444df2-6dd4-4fb9-b33a-eda37e15f887_3.png',
+      'imlunahey_the_water_is_showing_some_pink_tints_in_the_style_o_6d10258c-9dee-42f4-b5d9-f11435ace8ad_0.png',
+      'imlunahey_the_water_is_showing_some_pink_tints_in_the_style_o_6d10258c-9dee-42f4-b5d9-f11435ace8ad_1.png',
+      'imlunahey_the_water_is_showing_some_pink_tints_in_the_style_o_6d10258c-9dee-42f4-b5d9-f11435ace8ad_2.png',
+      'imlunahey_the_water_is_showing_some_pink_tints_in_the_style_o_6d10258c-9dee-42f4-b5d9-f11435ace8ad_3.png',
+      'imlunahey_this_photo_shows_the_ripple_of_water_and_colorful_p_f49f5639-d7b8-4ba7-a5ee-92b9f411afb2_0.png',
+      'imlunahey_this_photo_shows_the_ripple_of_water_and_colorful_p_f49f5639-d7b8-4ba7-a5ee-92b9f411afb2_1.png',
+      'imlunahey_this_photo_shows_the_ripple_of_water_and_colorful_p_f49f5639-d7b8-4ba7-a5ee-92b9f411afb2_2.png',
+      'imlunahey_this_photo_shows_the_ripple_of_water_and_colorful_p_f49f5639-d7b8-4ba7-a5ee-92b9f411afb2_3.png',
+    ]
+      .sort(() => Math.random() - 0.5)
+      .slice(0, 20)
+      .map((src) => {
+        const image = new Image();
+        image.src = `/backgrounds/${src}`;
+        return image;
+      });
   }, []);
 
   useEffect(() => {
@@ -661,11 +804,11 @@ export const ScreenshotTool = () => {
   const backgroundImagePicker = (
     <div className="flex flex-col gap-2">
       <label htmlFor="background-image-picker">Image</label>
-      <div className="flex flex-row gap-2">
+      <div className="grid grid-cols-5 gap-2">
         {images.current.map((image, index) => (
           <button
             key={index}
-            className="rounded-md p-2 text-white font-semibold"
+            className="rounded-md text-white font-semibold"
             onClick={() => {
               backgroundImage.current = image;
               setBackgroundImageSrc(image?.src ?? null);
@@ -674,7 +817,7 @@ export const ScreenshotTool = () => {
             <img
               src={image.src}
               alt={`Background image #${index}`}
-              className={cn('w-12 h-12 object-cover', {
+              className={cn('w-[48px] h-[48px] object-cover', {
                 'border border-black': backgroundImageSrc === image.src,
               })}
             />

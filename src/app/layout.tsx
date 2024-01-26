c4405ae66faea23e7104e8ef4def048986412392
luna <luna@wvvw.me>
2024-01-26 19:58:11 +1030

    fix: og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,10 @@ export default function RootLayout({
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <meta property="og:image" content="/opengraph-image.png" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="3020" />
+        <meta property="og:image:height" content="1714" />
         <link rel="manifest" href="/manifest.json" />
       </head>
       <body className={inter.className}>{children}</body>

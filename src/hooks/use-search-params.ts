40fc5887fa54e3f5e3a1f3760db3f3d2969eb804
luna <luna@wvvw.me>
2024-01-28 18:15:55 +1030

    fix: add debounce to useSearchParams

--- src/hooks/use-search-params.ts
+++ src/hooks/use-search-params.ts
@@ -4,10 +4,18 @@ import { useSearchParams as useNextSearchParams } from 'next/navigation';
 
 type Params = Record<string, boolean | number | string | null>;
 
+let timeout: ReturnType<typeof setTimeout>;
+const debounce = <T extends (...args: any[]) => any>(fn: T, delay: number) => {
+  return (...args: Parameters<T>): void => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => fn(...args), delay);
+  };
+};
+
 export const useSearchParams = <T extends Params>() => {
   const searchParams = useNextSearchParams();
 
-  const setSearchParams = (newParams: T) => {
+  const setSearchParams = debounce((newParams: T) => {
     const search = window ? window.location.search : '';
     const params = new URLSearchParams(search);
 
@@ -24,7 +32,7 @@ export const useSearchParams = <T extends Params>() => {
     if (newLocation !== window.location.href) {
       window.history.pushState({}, '', newLocation);
     }
-  };
+  }, 500);
 
   return [Object.fromEntries(searchParams.entries()), setSearchParams] as const;
 };

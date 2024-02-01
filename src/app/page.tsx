eec3d006707ddd1c974d8f4ac2010587714d7edd
luna <luna@wvvw.me>
2024-02-01 22:06:31 +1030

    fix: nextjs being a little bitch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 'use client';
+import { Suspense } from 'react';
 import { ScreenshotTool } from '../components/screenshot-tool';
 
 export default function Page() {
   return (
     <main className="flex flex-col gap-2">
-      <ScreenshotTool />
+      <Suspense>
+        <ScreenshotTool />
+      </Suspense>
     </main>
   );
 }

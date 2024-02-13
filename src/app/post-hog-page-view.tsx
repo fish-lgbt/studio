899aab12c61b93e9c6d2d9e039cf0d739c4fb463
luna <luna@wvvw.me>
2024-02-13 20:03:47 +1030

    fix: cleanup user auth

--- src/app/post-hog-page-view.tsx
+++ src/app/post-hog-page-view.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { usePathname, useSearchParams } from 'next/navigation';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import posthog from 'posthog-js';
 import { useConsent } from '@/hooks/use-consent';
 
@@ -14,7 +14,22 @@ const usePrevious = <T extends unknown>(value: T | null) => {
 };
 
 const useAuth = () => {
-  const userId = localStorage.getItem('anonId') ?? crypto.randomUUID();
+  const [userId, setUserId] = useState<string | null>(null);
+
+  // Watch for changes in the user id
+  useEffect(() => {
+    setUserId(localStorage.getItem('userId'));
+
+    const onChange = (event: StorageEvent) => {
+      if (event.key === 'userId') {
+        setUserId(event.newValue);
+      }
+    };
+
+    window.addEventListener('storage', onChange);
+    return () => window.removeEventListener('storage', onChange);
+  }, []);
+
   return { id: userId };
 };
 
@@ -26,6 +41,7 @@ export default function PostHogPageView(): null {
   const searchParams = useSearchParams();
   const consent = useConsent();
   const prevConsent = usePrevious(consent);
+  const [posthogInitalised, setPosthogInitalised] = useState<boolean>(false);
 
   // Create posthog instance
   useEffect(() => {
@@ -35,22 +51,35 @@ export default function PostHogPageView(): null {
     // If the user has accepted consent create a posthog instance
     if (consent === 'accepted') {
       // Start posthog
-      posthog.init(posthogApiKey, {
-        api_host: '/ingest',
-        persistence: 'cookie',
-      });
+      if (!posthogInitalised) {
+        posthog.init(posthogApiKey, {
+          api_host: '/ingest',
+          persistence: 'cookie',
+        });
+      }
+
+      // Set the user id
+      if (userId) {
+        posthog.identify(userId);
+      }
+
+      // Mark posthog as initialised
+      setPosthogInitalised(true);
     }
-  }, [consent, prevConsent, userId]);
+  }, [consent, posthogInitalised, prevConsent, userId]);
 
   // Track pageviews
   useEffect(() => {
-    if (pathname && posthog) {
-      const url = window.origin + pathname;
-      posthog.capture('$pageview', {
-        $current_url: searchParams ? `${url}?${searchParams.toString()}` : url,
-      });
-    }
-  }, [pathname, searchParams]);
+    if (!posthogInitalised) return;
+    if (!pathname) return;
+    if (!posthog) return;
+    if (consent !== 'accepted') return;
+
+    const url = window.origin + pathname;
+    posthog.capture('$pageview', {
+      $current_url: searchParams ? `${url}?${searchParams.toString()}` : url,
+    });
+  }, [consent, pathname, posthogInitalised, searchParams]);
 
   return null;
 }
